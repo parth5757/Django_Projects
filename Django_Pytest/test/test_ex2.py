31ab0f460488e04f0ba8eca84b706878dc8a21b1
parth5757 <psthakkar2@gmail.com>
2024-10-10 17:19:17 +0530

    django pytest text_ex2.py

--- Django_Pytest/test/test_ex2.py
+++ Django_Pytest/test/test_ex2.py
@@ -1,22 +1,12 @@
-# import pytest
-# # fixture
-# # @pytest.fixture
-# # def fixture_1(scope="session"):
-# #     print('run-fixture-1')
-# #     return 1
+# In this test show yield use. (pytest have separate yield fixture. which are not here use.)
 
-# # def test_example1(fixture_1):
-# #     print("run-example-1")  
-# #     num = fixture_1
-# #     assert num == 1
-    
+import pytest
+@pytest.fixture
+def yield_fixture():
+    print("start Test Phase")
+    yield 6
+    print("End Test")
 
-# @pytest.fixture
-# def yield_fixture():
-#     print("start Test Phase")
-#     yield 6
-#     print("End Test")
-
-# def test_example1(yield_fixture):
-#     print("run-example-1")  
-#     assert yield_fixture == 6
+def test_example1(yield_fixture):
+    print("run-example-1")  
+    assert yield_fixture == 6
