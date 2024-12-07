3cf7bae338c53fce2f986e0af1472bb6c9abfb83
parth5757 <psthakkar2@gmail.com>
2024-12-07 09:04:41 +0530

    configure default Authentication classes of Django DRF

--- DJango_React_chat/djchat/djchat/settings.py
+++ DJango_React_chat/djchat/djchat/settings.py
@@ -139,6 +139,9 @@ AUTH_USER_MODEL = "account.Account"
 REST_FRAMEWORK = {
     # YOUR SETTINGS
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_AUTHENTICATION_CLASSES":[
+        "rest_framework.authentication.SessionAuthentication",
+    ]
 }
 
 SPECTACULAR_SETTINGS = {
