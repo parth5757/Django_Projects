f8d0b17ff9a57a19e12d2f35ab97dae0984981f8
parth5757 <psthakkar2@gmail.com>
2024-12-11 13:18:07 +0530

    DRF endpoint api filtering serves by server id

--- DJango_React_chat/djchat/server/views.py
+++ DJango_React_chat/djchat/server/views.py
@@ -1,42 +1,26 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializer import ServerSerializer
-from .models import Server
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from .models import Server
+from .serializer import ServerSerializer
 
 class ServerListViewSet(viewsets.ViewSet):
 
     queryset = Server.objects.all()
-
-    # def list(self, request):
-
-    #     category = request.query_params.get("category")
-    #     qty = request.query_params.get("qty")
-    #     by_user = request.query_params.get("by_user") == "true"
-
-    #     # Start with the base queryset
-    #     queryset = self.queryset
-
-    #     if category:
-    #         self.queryset.filter(category=category) 
-
-    #     if by_user:
-    #         user_id = request.user.id
-    #         self.queryset = self.queryset.filter(member=user_id)
-
-    #     if qty:
-    #         self.queryset = self.queryset[: int[qty]]
-
-    #     serializer = ServerSerializer(self.queryset, many=True)
-    #     return Response(serializer.data)
     
     def list(self, request):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
-
+        by_serverid = request.query_params.get("by_serverid")
         # Start with the base queryset
         queryset = self.queryset
+
+        # if by_user and not request.user.is_authenticated:
+        #     raise AuthenticationFailed()
+        if by_user or by_serverid and not request.user.is_authenticated:
+            raise AuthenticationFailed()
 
         if category:
             queryset = queryset.filter(category=category)
@@ -47,6 +31,16 @@ class ServerListViewSet(viewsets.ViewSet):
 
         if qty:
             queryset = queryset[: int(qty)]
+
+        if by_serverid:
+            try:
+                server_id = int(by_serverid)
+                queryset = queryset.filter(id=server_id)  # Filter by server ID
+                if not queryset.exists():
+                    raise ValidationError(detail=[f"Server with ID {server_id} not found"])
+            except ValueError:
+                raise ValidationError(detail=[f"Server value error"])
+
 
         serializer = ServerSerializer(queryset, many=True)
         return Response(serializer.data)
