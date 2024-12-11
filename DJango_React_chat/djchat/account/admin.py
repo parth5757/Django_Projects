cb2752c3c5a5cbf8bab1f59b2216dc343ca49a2c
parth5757 <psthakkar2@gmail.com>
2024-12-11 11:12:35 +0530

    Django endpoint api serve by user authentiaction added

--- DJango_React_chat/djchat/account/admin.py
+++ DJango_React_chat/djchat/account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Account
+
+admin.site.register(Account, UserAdmin)
+
+
--- DJango_React_chat/djchat/server/views.py
+++ DJango_React_chat/djchat/server/views.py
@@ -8,12 +8,45 @@ class ServerListViewSet(viewsets.ViewSet):
 
     queryset = Server.objects.all()
 
+    # def list(self, request):
+
+    #     category = request.query_params.get("category")
+    #     qty = request.query_params.get("qty")
+    #     by_user = request.query_params.get("by_user") == "true"
+
+    #     # Start with the base queryset
+    #     queryset = self.queryset
+
+    #     if category:
+    #         self.queryset.filter(category=category) 
+
+    #     if by_user:
+    #         user_id = request.user.id
+    #         self.queryset = self.queryset.filter(member=user_id)
+
+    #     if qty:
+    #         self.queryset = self.queryset[: int[qty]]
+
+    #     serializer = ServerSerializer(self.queryset, many=True)
+    #     return Response(serializer.data)
+    
     def list(self, request):
         category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
+        by_user = request.query_params.get("by_user") == "true"
+
+        # Start with the base queryset
+        queryset = self.queryset
 
         if category:
-            self.queryset.filter(category__name = category) 
+            queryset = queryset.filter(category=category)
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        if by_user:
+            user_id = request.user.id
+            queryset = queryset.filter(member=user_id)
 
+        if qty:
+            queryset = queryset[: int(qty)]
+
+        serializer = ServerSerializer(queryset, many=True)
         return Response(serializer.data)
